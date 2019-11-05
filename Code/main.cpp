f9670294ead1f9e5fe38ba9554e9335fb840e3d4
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-11-05 14:45:12 -0300

    Reading inputs

---
+++ Code/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+//#include <chrono>
+
+//using namespace std::chrono;
+using std::ifstream;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char** argv){
+    if(argc > 1){ //Checks if the file name was passed as a program parameter
+
+        string fileName = argv[1];
+    
+        ifstream input(fileName);
+
+        int square, line, column;
+        int **matrix;
+
+
+        if(input.is_open()){
+            input >> square  >> line >> column;
+
+            matrix = new int*[square];
+            for(int i = 0; i < square; i++){
+                matrix[i] = new int[square];
+            }
+            
+            for(int i = 0; i < square; i++){
+                for(int j = 0; j < square; j++){
+                    input >> matrix[i][j];
+                }
+            }
+        }
+        else{
+            cout << "Couldn't open file " << fileName << 
+                ", please check if the file name is correct and if it actually exists" << endl;;
+        }
+    }
+    else{
+        cout << "Please pass a file name as a parameter to the program" << endl;
+    }
+     
+}
